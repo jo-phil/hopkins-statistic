687b270e0689e74056bca54e149b2938c7bfa4c7
Johann Philipp Gorzny <93314246+jo-phil@users.noreply.github.com>
2025-12-06 22:32:39 +0100

    build: initialize project structure

---
+++ src/hopkins_statistic/__init__.py
@@ -0,0 +1,5 @@
+"""Hopkins statistic: a measure of clustering tendency.
+
+Compute the Hopkins statistic to test for departure from complete spatial
+randomness (CSR), commonly used to assess clustering tendency.
+"""
