6d1148f2c7236ace260712ce3fbb16f52fe088b9
Johann Philipp Gorzny <93314246+jo-phil@users.noreply.github.com>
2025-12-23 00:55:52 +0100

    test: add README example test
    Add a test to validate the code example in README.md.
    Remove tests that are redundant with doctest tests.

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,8 +1,6 @@
 import pytest
 from pytest_examples import find_examples
 
-from hopkins_statistic import hopkins
-
 
 @pytest.mark.parametrize("example", find_examples("README.md"), ids=str)
 def test_examples(example, eval_example):
