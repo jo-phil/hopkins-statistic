65c52978dc12f9ebeccd759b5f4e27044f3db17e
Johann Philipp Gorzny <93314246+jo-phil@users.noreply.github.com>
2026-01-18 16:49:43 +0100

    test: add toroidal test cases
    Parameterize existing behavior tests with the `toroidal` parameter.
    Ensure vertices of the hyperrectangle overlap in toroidal topology.
    Test invariance of applying toroidal with doubled box size.
    Check that non-bool `toroidal` raises an exception.

--- tests/test_behavior.py
+++ tests/test_behavior.py
@@ -1,4 +1,5 @@
 import itertools
+from unittest.mock import patch
 
 import numpy as np
 import pytest
@@ -7,6 +8,11 @@ from scipy.stats import beta
 from hopkins_statistic import hopkins, hopkins_test
 
 N, D = 100, 2  # smallest reasonable shape for behavioral tests
+
+
+@pytest.fixture(params=[False, True], ids=["euclidean", "toroidal"])
+def toroidal(request):
+    return request.param
 
 
 @pytest.fixture
@@ -20,34 +26,40 @@ def seed(request):
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("d", [2, 3, 5])
-def test_beta_moments_under_uniform_null(d, rng):
+@pytest.mark.parametrize("d", [2, 3, 5], ids=lambda val: f"d={val}")
+def test_null(d, toroidal, rng):
     m = N // 10
     Xs = rng.uniform(size=(1000, N, d))
-    Hs = [hopkins(X, m=m, rng=rng) for X in Xs]
-    assert np.mean(Hs) == pytest.approx(0.5, abs=0.05)
-    assert np.std(Hs) == pytest.approx(beta.std(m, m), abs=0.05)
+    Hs = [hopkins(X, m=m, toroidal=toroidal, rng=rng) for X in Xs]
+
+    # Using toroidal topology, the empirical distribution of the
+    # statistic is closer to the theoretical beta distribution.
+    tol = 0.01 if toroidal else 0.05
+
+    assert np.mean(Hs) == pytest.approx(0.5, abs=tol)
+    assert np.std(Hs) == pytest.approx(beta.std(m, m), abs=tol)
 
 
-def test_low_under_regularity(seed):
-    X = np.array(list(itertools.product(range(int(N ** (1 / D))), repeat=D)))
-
-    result = hopkins_test(X, alternative="regular", rng=seed)
+def test_regularity(toroidal, seed):
+    X = np.array(list(itertools.product(range(N), repeat=D)))
+    result = hopkins_test(
+        X, toroidal=toroidal, alternative="regular", rng=seed
+    )
     assert result.statistic < 0.3
     assert result.pvalue < 0.001
 
-    heuristic = hopkins(X, power=1, rng=seed)
+    heuristic = hopkins(X, toroidal=toroidal, power=1, rng=seed)
     assert heuristic != pytest.approx(result.statistic)
-    assert heuristic < 0.35
+    assert heuristic < 0.3
 
 
-def test_high_under_clustering(seed):
+def test_clustering(toroidal, seed):
     rng = np.random.default_rng(seed)
     corners = np.array(list(itertools.product([0, 1], repeat=D)))
     indices = rng.integers(len(corners), size=N)
     X = (corners[indices] + rng.normal(scale=0.1, size=(N, D))) % 1
 
-    result = hopkins_test(X, rng=seed)
+    result = hopkins_test(X, toroidal=toroidal, rng=seed)
     assert result.statistic > 0.7
     assert result.pvalue < 0.001
 
@@ -56,13 +68,27 @@ def test_high_under_clustering(seed):
     assert heuristic > 0.7
 
 
-def test_one_under_extreme_clustering(rng):
+def test_extreme_clustering(toroidal):
     X = [[0, 0], [1, 1]] * 2
-    assert hopkins(X, rng=rng) == 1.0
+    assert hopkins(X, toroidal=toroidal) == 1.0
 
 
-def test_invariant_under_scale_and_shift(rng):
+def test_toroidal_vertices(rng):
+    X = list(itertools.product([0, 1], repeat=7))
+    assert hopkins(X, toroidal=False, rng=rng) < 0.3
+    assert hopkins(X, toroidal=True) == 1.0
+
+
+def test_toroidal_invariance(rng):
     X = rng.uniform(size=(N, D))
-    H1 = hopkins(X, rng=42)
-    H2 = hopkins(2 * X + 1, rng=42)
+    H1 = hopkins(X, toroidal=False, rng=42)
+    with patch("numpy.nextafter", new=lambda x, _: 2 * x):
+        H2 = hopkins(X, toroidal=True, rng=42)
+    assert H2 == pytest.approx(H1)  # noqa: SIM300
+
+
+def test_scale_and_shift_invariance(toroidal, rng):
+    X = rng.uniform(size=(N, D))
+    H1 = hopkins(X, toroidal=toroidal, rng=42)
+    H2 = hopkins(2 * X + 1, toroidal=toroidal, rng=42)
     assert H2 == pytest.approx(H1)  # noqa: SIM300
