87c121aec81cf353b03cbefdfeadb215381702d9
Johann Philipp Gorzny <93314246+jo-phil@users.noreply.github.com>
2026-03-26 17:14:24 +0100

    test: extend invariance tests to hopkins_test
    Run invariance/equivalence tests also on hopkins_test using the existing
    parameterized `hopkins_func` fixture to improve coverage.
    Rename variables to improve readability.

--- tests/test_invariance.py
+++ tests/test_invariance.py
@@ -2,34 +2,32 @@ from unittest.mock import patch
 
 import pytest
 
-from hopkins_statistic import hopkins
-
 
 @pytest.fixture
 def X(rng):
     return rng.uniform(size=(100, 2))
 
 
-def test_shift_invariance(X, toroidal):
-    H1 = hopkins(X, toroidal=toroidal, rng=42)
-    H2 = hopkins(X - 2, toroidal=toroidal, rng=42)
-    assert H2 == pytest.approx(H1)  # noqa: SIM300
+def test_shift_invariance(hopkins_func, X, toroidal):
+    baseline = hopkins_func(X, toroidal=toroidal, rng=42)
+    shifted = hopkins_func(X - 2, toroidal=toroidal, rng=42)
+    assert shifted == pytest.approx(baseline)
 
 
-def test_scale_invariance(X, toroidal):
-    H1 = hopkins(X, toroidal=toroidal, rng=42)
-    H2 = hopkins(2 * X, toroidal=toroidal, rng=42)
-    assert H2 == pytest.approx(H1)  # noqa: SIM300
+def test_scale_invariance(hopkins_func, X, toroidal):
+    baseline = hopkins_func(X, toroidal=toroidal, rng=42)
+    scaled = hopkins_func(2 * X, toroidal=toroidal, rng=42)
+    assert scaled == pytest.approx(baseline)
 
 
 def test_explicit_bbox_equivalence(hopkins_func, X):
-    baseline = hopkins_func(X, frame="bbox", rng=42)
+    implicit = hopkins_func(X, frame="bbox", rng=42)
     explicit = hopkins_func(X, frame=(X.min(axis=0), X.max(axis=0)), rng=42)
-    assert explicit == baseline
+    assert explicit == implicit
 
 
-def test_toroidal_non_wrapping_equivalence(X):
-    H1 = hopkins(X, toroidal=False, rng=42)
+def test_toroidal_non_wrapping_equivalence(hopkins_func, X):
+    non_toroidal = hopkins_func(X, toroidal=False, rng=42)
     with patch("numpy.nextafter", new=lambda x, _: 2 * x):
-        H2 = hopkins(X, toroidal=True, rng=42)
-    assert H2 == pytest.approx(H1)  # noqa: SIM300
+        toroidal = hopkins_func(X, toroidal=True, rng=42)
+    assert toroidal == pytest.approx(non_toroidal)
