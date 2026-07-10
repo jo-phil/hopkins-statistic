accbf7ea8ef37e89004f6d17e9c19ea3c03d81e7
Johann Philipp Gorzny <jo-phil@gorzny.de>
2026-07-10 14:01:55 +0200

    test: convert parametrization values to list (#39)
    Fix the deprecation warning for passing a non-collection iterable to
    `pytest.mark.parametrize`.

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -2,6 +2,6 @@ import pytest
 from pytest_examples import find_examples
 
 
-@pytest.mark.parametrize("example", find_examples("README.md"), ids=str)
+@pytest.mark.parametrize("example", list(find_examples("README.md")), ids=str)
 def test_examples(example, eval_example):
     eval_example.run_print_check(example)
